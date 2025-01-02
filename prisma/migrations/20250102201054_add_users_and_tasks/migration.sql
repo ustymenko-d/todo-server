ab0f85940b0856575d7f0a3d7aaaf9b6a5ddc59e
ustymenko-d <ustymenko.denys@gmail.com>
2025-01-02 22:24:41 +0200

    Init app with basic structure and configure db

---
+++ prisma/migrations/20250102201054_add_users_and_tasks/migration.sql
@@ -0,0 +1,35 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('IN_PROGRESS', 'COMPLETE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "status" "TaskStatus" NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "parentTaskId" INTEGER,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_parentTaskId_fkey" FOREIGN KEY ("parentTaskId") REFERENCES "Task"("id") ON DELETE SET NULL ON UPDATE CASCADE;
