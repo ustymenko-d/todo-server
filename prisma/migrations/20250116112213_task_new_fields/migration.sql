c014b4d47b984a84e18b38b3f8494796c979b87f
ustymenko-d <ustymenko.denys@gmail.com>
2025-01-16 13:24:57 +0200

    Prisma Task schema update

---
+++ prisma/migrations/20250116112213_task_new_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "description" TEXT,
+ADD COLUMN     "expiresAt" TIMESTAMP(3);
