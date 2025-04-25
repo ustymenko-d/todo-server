e82c58b500a100bdbe73b23bd8e304491a42d623
ustymenko-d <ustymenko.denys@gmail.com>
2025-04-25 15:16:19 +0300

    Delete MailModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,21 +4,20 @@ import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PasswordModule } from './password/password.module';
 import { CookiesModule } from './cookies/cookies.module';
-import { MailModule } from './mail/mail.module';
 import { TokensModule } from './tokens/tokens.module';
 import { JwtStrategy } from 'src/common/strategies/jwt.strategy';
 import { FoldersService } from 'src/folders/folders.service';
+import { MailService } from './mail/mail.service';
 
 @Module({
   imports: [
     PrismaModule,
     forwardRef(() => PasswordModule),
     CookiesModule,
-    MailModule,
     TokensModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, FoldersService],
+  providers: [AuthService, JwtStrategy, MailService, FoldersService],
   exports: [AuthService],
 })
 export class AuthModule {}
