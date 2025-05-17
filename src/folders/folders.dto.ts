9ed6f2d0aa1978aaee184bc5bb517218762013c1
ustymenko-d <ustymenko.denys@gmail.com>
2025-05-17 21:24:44 +0300

    Update folder name validation regex and message to allow underscores and dots

--- src/folders/folders.dto.ts
+++ src/folders/folders.dto.ts
@@ -12,8 +12,9 @@ export class FolderName {
   @MaxLength(25, {
     message: 'The folder name must not exceed 25 characters',
   })
-  @Matches(/^[\p{L}0-9 _-]+$/u, {
-    message: 'The folder name must contain only letters, digits and spaces.',
+  @Matches(/^[\p{L}0-9 _\-.]+$/u, {
+    message:
+      'The folder name may only contain letters, digits, spaces, dashes, underscores, and dots.',
   })
   name: string;
 }
