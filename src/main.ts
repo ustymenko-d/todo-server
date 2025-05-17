ac3187164fa116a65114486a1b51aeb2f5636cdc
ustymenko-d <ustymenko.denys@gmail.com>
2025-05-17 17:39:57 +0300

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const frontendUrl = configService.get<string>('FRONTEND_URL');
+
+  configureApp(app, configService);
+
   const port = configService.get<number>('PORT') ?? 8080;
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-  app.use(cookieParser());
-  app.enableCors({
-    origin: frontendUrl,
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-    credentials: true,
-  });
-
   await app.listen(port);
 }
+
+function configureApp(app: INestApplication, configService: ConfigService) {
+  app.useGlobalPipes(getValidationPipe());
+  app.use(cookieParser());
+  app.enableCors(getCorsOptions(configService.get('FRONTEND_URL')));
+}
+
+const getValidationPipe = (): ValidationPipe =>
+  new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  });
+
+const getCorsOptions = (origin?: string) => ({
+  origin: origin ?? '*',
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  credentials: true,
+});
 
 bootstrap();
