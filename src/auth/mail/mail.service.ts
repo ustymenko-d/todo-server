e03af9f5d53d3dc68555b61e3e528654352d686e
ustymenko-d <ustymenko.denys@gmail.com>
2025-08-04 13:49:23 +0300

    Update project name and description

--- src/auth/mail/mail.service.ts
+++ src/auth/mail/mail.service.ts
@@ -49,9 +49,9 @@ export class MailService {
     verificationToken: string,
   ): Promise<void> {
     const verificationUrl = `${this.frontendUrl}/verification?verificationToken=${verificationToken}`;
-    const html = `<!doctype html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Verify Your Email</title></head><body style="font-family:Arial,sans-serif;padding:20px;color:#09090b"><div style="max-width:600px;margin:0 auto;background:#fafafa;padding:20px;border-radius:8px;box-shadow:0 4px 6px rgba(0,0,0,.1);text-align:center;border:1px solid #d4d4d8"><h2 style="color:#09090b">Welcome to UpTodo!</h2><p style="color:#09090b">Thank you for creating your account! Please verify your email using the link below. If you don’t verify your email within three days, your account will be deleted.</p><a href="${verificationUrl}" style="display:inline-block;padding:10px 20px;background-color:#18181b;color:#fff;text-decoration:none;border-radius:6px;font-weight:700">Verify Email</a></div></body></html>`;
+    const html = `<!doctype html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Verify Your Email</title></head><body style="font-family:Arial,sans-serif;padding:20px;color:#09090b"><div style="max-width:600px;margin:0 auto;background:#fafafa;padding:20px;border-radius:8px;box-shadow:0 4px 6px rgba(0,0,0,.1);text-align:center;border:1px solid #d4d4d8"><h2 style="color:#09090b">Welcome to uTodo!</h2><p style="color:#09090b">Thank you for creating your account! Please verify your email using the link below. If you don’t verify your email within three days, your account will be deleted.</p><a href="${verificationUrl}" style="display:inline-block;padding:10px 20px;background-color:#18181b;color:#fff;text-decoration:none;border-radius:6px;font-weight:700">Verify Email</a></div></body></html>`;
 
-    await this.sendEmail(email, `Verify your email on UpTodo`, html);
+    await this.sendEmail(email, `Verify your email on uTodo`, html);
   }
 
   async sendResetPasswordEmail(
@@ -60,8 +60,8 @@ export class MailService {
   ): Promise<void> {
     const resetUrl = `${this.frontendUrl}/auth/reset-password?resetToken=${resetToken}`;
     const html = `
-      <!doctype html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Reset Your UpTodo password</title></head><body style="font-family:Arial,sans-serif;padding:20px;color:#09090b"><div style="max-width:600px;margin:0 auto;background:#fafafa;padding:20px;border-radius:8px;box-shadow:0 4px 6px rgba(0,0,0,.1);text-align:center;border:1px solid #d4d4d8"><h2 style="color:#09090b">Reset Your UpTodo password</h2><p style="color:#09090b">This link will expire after 30 minutes. If you didn't request a password reset you can delete this email.</p><a href="${resetUrl}" style="display:inline-block;padding:10px 20px;background-color:#18181b;color:#fff;text-decoration:none;border-radius:6px;font-weight:700">Reset password</a></div></body></html>`;
+      <!doctype html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Reset Your uTodo password</title></head><body style="font-family:Arial,sans-serif;padding:20px;color:#09090b"><div style="max-width:600px;margin:0 auto;background:#fafafa;padding:20px;border-radius:8px;box-shadow:0 4px 6px rgba(0,0,0,.1);text-align:center;border:1px solid #d4d4d8"><h2 style="color:#09090b">Reset Your uTodo password</h2><p style="color:#09090b">This link will expire after 30 minutes. If you didn't request a password reset you can delete this email.</p><a href="${resetUrl}" style="display:inline-block;padding:10px 20px;background-color:#18181b;color:#fff;text-decoration:none;border-radius:6px;font-weight:700">Reset password</a></div></body></html>`;
 
-    await this.sendEmail(email, `Reset Your UpTodo password`, html);
+    await this.sendEmail(email, `Reset Your uTodo password`, html);
   }
 }
